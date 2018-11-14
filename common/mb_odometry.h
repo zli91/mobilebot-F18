2f257dffc342f1f484bb431375bbae928ecdbb1f
pgaskell <pgaskell@umich.edu>
2018-11-13 22:07:33 -0500

    Initial commit

---
+++ common/mb_odometry.h
@@ -0,0 +1,14 @@
+/*******************************************************************************
+* mb_odometry.h
+*
+* 
+*******************************************************************************/
+#ifndef MB_ODOMETRY_H
+#define MB_ODOMETRY_H
+#include "mb_structs.h"
+
+void mb_initialize_odometry(mb_odometry_t* mb_odometry, float x, float y, float theta);
+void mb_update_odometry(mb_odometry_t* mb_odometry, mb_state_t* mb_state);
+float mb_clamp_radians(float angle);
+
+#endif
