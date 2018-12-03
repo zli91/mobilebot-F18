2d20db16b3a154d6fb1e641f5195a36feb8675d2
pgaskell <pgaskell@umich.edu>
2018-12-03 15:19:09 -0500

    fixed angle difference

--- common/mb_odometry.h
+++ common/mb_odometry.h
@@ -10,5 +10,6 @@
 void mb_initialize_odometry(mb_odometry_t* mb_odometry, float x, float y, float theta);
 void mb_update_odometry(mb_odometry_t* mb_odometry, mb_state_t* mb_state);
 float mb_clamp_radians(float angle);
+float mb_angle_diff_radians(float angle1, float angle2);
 
 #endif
